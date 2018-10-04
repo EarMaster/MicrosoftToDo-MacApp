fba3665ba186ab73537a9f562891d1406bc60da5
Nico Wiedemann <nico.wiedemann@visionsbox.de>
2018-10-04 18:10:46 +0200

    Renamed Icon

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-nativefier --name "Microsoft To-Do" --icon icon-office.png --inject notifications.js --counter --internal-urls ".*\.(live|office|microsoft)\.com.*" "https://to-do.office.com/" ./dist
+nativefier --name "Microsoft To-Do" --icon icon.png --inject notifications.js --counter --internal-urls ".*\.(live|office|microsoft)\.com.*" "https://to-do.office.com/" ./dist
